8dae843deecd24ad43d30f936cace5704d264f48
Luke <coeurdeluke.js@gmail.com>
2025-05-17 12:53:24 -0300

    Cuarto Commit: Proyecto Crypto Force Domains

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',  // Cambia esto de '/' a './'
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
   server: {
     hmr: {
       overlay: false
